7b7f8a7c4b6963175e6cad933af75cfcef2b89cc
ravichandran1383 <contact@bigspire.com>
2017-09-27 19:56:58 +0530

    changes

--- mysql_test.php
+++ mysql_test.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect(null,'root','como#9!','commodb','','/cloudsql/commo-181207:us-central1:commodb');
+$con = mysqli_connect('/cloudsql/commo-181207:us-central1:commodb','root','como#9!','commodb');
 if($con) 
 echo 'connected';
 die(mysql_error());
