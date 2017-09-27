9a2781775073b72b9929b407d3803fc23f17503f
ravichandran1383 <contact@bigspire.com>
2017-09-27 19:36:23 +0530

    changes

--- mysql_test.php
+++ mysql_test.php
@@ -1,9 +1,11 @@
 <?php
-$dsn = getenv('MYSQL_DNS');
-$user = getenv('MYSQL_USER');
-$password = getenv('MYSQL_PASSWORD');
+echo $dsn = getenv('MYSQL_DNS');
+echo  $user = getenv('MYSQL_USER');
+echo $password = getenv('MYSQL_PASSWORD');
 $con = mysql_connect($dsn,$user,$password);
 if($con) 
 echo 'connected';
 die(mysql_error());
+phpinfo();
 ?>
+
