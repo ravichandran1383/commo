94162626ab5975d5a734aa39d743a038e9d0bab6
ravichandran1383 <contact@bigspire.com>
2017-09-27 19:29:03 +0530

    changes

--- mysql_test.php
+++ mysql_test.php
@@ -1,5 +1,8 @@
 <?php
-$con = mysql_connect('commo-181207:us-central1:commodb', 'root', 'como#9!');
+$dsn = getenv('MYSQL_DNS');
+$user = getenv('MYSQL_USER');
+$password = getenv('MYSQL_PASSWORD');
+$con = mysql_connect($dsn,$user,$password);
 if($con) 
 echo 'connected';
 die(mysql_error());
